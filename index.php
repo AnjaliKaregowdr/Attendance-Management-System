2b8c2c5b2dc7a9b8d76b33cd1f13e062e42e8e84
Anjali K M <83869891+AnjaliKaregowdr@users.noreply.github.com>
2021-10-06 20:05:29 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,101 @@
-<?php include 'modules/header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>ONLINE ATTENDANCE</title>
 
- <?php include 'modules/nav.php';?>
-<?php include 'modules/content-container.php';?>
-<?php // include 'modules/a1.php';?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php include 'modules/footer.php';?>
-<html>
-<body>
-  <header >
-
-
-
-</header>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/icon" href="img/wpf-favicon.png"/>
 
 
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font awesome css file-->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Superslide css file-->
+    <link rel="stylesheet" href="css/superslides.css">
+    <!-- Slick slider css file -->
+    <link href="css/slick.css" rel="stylesheet">
+    <!-- Circle counter cdn css file -->
+    <link rel='stylesheet prefetch' href='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/css/jquery.circliful.css'>
+    <!-- smooth animate css file -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- preloader -->
+    <link rel="stylesheet" href="css/queryLoader.css" type="text/css" />
+    <!-- gallery slider css -->
+    <link type="text/css" media="all" rel="stylesheet" href="css/jquery.tosrus.all.css" />
+    <!-- Default Theme css file -->
+    <link id="switcher" href="css/themes/default-theme.css" rel="stylesheet">
+    <!-- Main structure css file -->
+    <link href="style.css" rel="stylesheet">
 
+    <!-- Google fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
+
+  </head>
+  <body>
+
+    <a class="scrollToTop" href="#"></a>
+    <header id="header">
+
+    </header>
+    <section id="slider">
+      <div class="row">
+        <div class="col-lg-12 col-md-12">
+          <div class="slider_area">
+            <!-- Start super slider -->
+            <div id="slides">
+              <ul class="slides-container">
+                <li>
+                  <img src="img/slider/2.jpg" alt="img">
+                   <div class="slider_caption">
+                    <h2>ONLINE ATTENDANCE SYSTEM</h2>
+                    <a class="slider_btn" href="../ls.php">ADMIN AREA</a>
+					<a class="slider_btn" href="../index.php">TEACHER AND STUDENT AREA</a>
+                  </div>
+
+                  </li>
+                <li>
+                  <img src="img/slider/3.jpg" alt="img">
+                   <div class="slider_caption slider_right_caption">
+                    <h2>ONLINE ATTENDANCE SYSTEM</h2>
+                    <a class="slider_btn" href="../ls.php">ADMIN AREA</a>
+					<a class="slider_btn" href="../index.php">TEACHER AND STUDENT AREA</a>
+                  </div>
+                </li>
+                
+                <li>
+                  <img src="img/slider/4.jpg" alt="img">
+                   <div class="slider_caption">
+                    <h2>ONLINE ATTENDANCE SYSTEM</h2>
+                    <a class="slider_btn" href="../ls.php">ADMIN AREA</a>
+					<a class="slider_btn" href="../index.php">TEACHER AND STUDENT AREA</a>
+                  </div>
+                </li>
+              </ul>
+              <nav class="slides-navigation">
+                <a href="#" class="next"></a>
+                <a href="#" class="prev"></a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/queryloader2.min.js" type="text/javascript"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.animate-enhanced.min.js"></script>
+    <script src="js/jquery.superslides.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/js/jquery.circliful.min.js'></script>
+    <script type="text/javascript" language="javascript" src="js/jquery.tosrus.min.all.js"></script>
+
+    <script src="js/custom.js"></script>
   </body>
 </html>
